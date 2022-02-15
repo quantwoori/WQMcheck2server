33dfa182d954c7355acc7f8c5b38c541ebf2dc06
quantwoori <sangil.bae@wooriam.kr>
2022-02-15 09:30:03 +0900

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='check2server',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Check',
+    author_email='baesangil0906@gmail.com',
+    description='Insert Check Data into Database'
+)
