c95402f2414b88ad7e99aa69590f2d8ea9a4601f
quantwoori <sangil.bae@wooriam.kr>
2022-02-23 11:28:27 +0900

    monday - friday chk afternoon check

--- main_data.py
+++ main_data.py
@@ -4,10 +4,19 @@ from datetime import datetime, timedelta
 
 if __name__ == "__main__":
     cd = CheckData()
-    dt = (datetime.today() - timedelta(days=1)).strftime('%Y%m%d')
+    if datetime.now().strftime('%H%M%S') > '170000':
+        dt= datetime.today().strftime('%Y%m%d')
+    else:
+        dt = (datetime.today() - timedelta(days=1)).strftime('%Y%m%d')
+
+    if datetime.now().strftime('%A') == 'Monday':
+        # Find Friday
+        dt = (datetime.today() - timedelta(days=3)).strftime('%Y%m%d')
+
+
 
     cd.xl_cell_input(
-        start_date="20220214",
+        start_date=dt,
         end_date=dt,
         stk_codes=cd.kospi.k100['STK_CODE'].values()
     )
